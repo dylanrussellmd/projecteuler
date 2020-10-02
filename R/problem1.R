5de64670cd77de94678faee196c92a2d200abb9e
Dylan Russell <dyl.russell@gmail.com>
2020-10-01 17:29:35 -1000

    p1

--- R/problem1.R
+++ R/problem1.R
@@ -1,18 +1,42 @@
 #' Problem 1 - Multiples of 3 and 5
 #'
-#'
-#' @details
 #' If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-#'
 #' Find the sum of all the multiples of 3 or 5 below 1000.
 #'
-
-p1 <- function(n, ...) {
- dots <- as.integer(c(...))
- sets <- purrr::map(dots, function(i) {
-   seq(i, n-1L, i)
- })
- sum(sapply(sets, length))
+#' @return a double vector
+#'
+#' @export
+#' @examples
+#' p1()
+#'
+p1 <- function() {
+  sumFactors(1000, 3, 5)
 }
+
+#' @param n a natural number below which (non-inclusive) to sum all multiples of given numbers
+#' @param ... natural numbers below `n` that will serve as multiples to sum
+#'
+#' @details
+#' # sumFactors
+#'
+#' This function generalizes the answer to Project Euler problem #1. It is a simple way of
+#' implementing the \href{https://www.wikiwand.com/en/Inclusion%E2%80%93exclusion_principle}{inclusion-exclusion principle}
+#' in R. Given any arbitrary length vector of natural numbers (`...`) and a limit (`n`), the sum of all multiples of the
+#' provided numbers can be obtained.
+#'
+#' @describeIn p1 generic function for Project Euler problem #1
+#' @export
+#' @examples
+#' sumFactors(1000, 3, 5)
+sumFactors <- function(n, ...) {
+ lapply(c(...), function(i) {
+   seq(i, n - 1L, i)
+ }) %>%
+    unlist %>%
+    unique %>%
+    sum
+}
+
+
 
 
---
+++ README.md
@@ -0,0 +1,34 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Project EuleR
+
+<!-- badges: start -->
+
+[![Travis build
+status](https://travis-ci.com/dylanrussellmd/projecteuler.svg?branch=master)](https://travis-ci.com/dylanrussellmd/projecteuler)
+[![R build
+status](https://github.com/dylanrussellmd/projecteuler/workflows/R-CMD-check/badge.svg)](https://github.com/dylanrussellmd/projecteuler/actions)
+<!-- badges: end -->
+
+**WARNING - THIS PACKAGE CONTAINS SOLUTIONS TO PROJECT EULER PROBLEMS**
+
+Welcome to Project Eule**R**\! My hope is this will become the
+gold-standard repository for R language solutions to all Project Euler
+problems. Download the package and associated vignettes with the
+following code snippet:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("dylanrussellmd/projecteuler")
+```
+
+Follow along as I begin a journey one-by-one through the Project Euler
+archived problems. I will work exclusively in R but no package is
+off-limits\!
+
+**Collaboration is encouraged**\! If you think you can do it better,
+faster, or more elegantly, *please make a pull request*.
+
+Sign up for your own [Project Euler](https://projecteuler.net/) account
+and try it out.
